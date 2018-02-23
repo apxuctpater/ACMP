0f3657b762826bd98b35897b88c47c5bc0f35657
apxuctpater <apxuctpater@gmail.com>
2018-02-23 16:36:17 +0300

    T13

--- src/ACMP/Task12.java
+++ src/ACMP/Task12.java
@@ -13,36 +13,41 @@ public class Task12 {
     public static void solve(Scanner in, PrintWriter out){
 
         int n = in.nextInt();
-        long x = 0;
-        long y = 0;
+        int x = 0;
+        int y = 0;
         int x1 = 0;
         int y1 = 0;
         int x3 = 0;
         int y3 = 0;
-        ArrayList<String> coords = new ArrayList<>(n);
-            for(int i = 0; i <= n; i++){
-                        coords.add(in.nextLine());
-                }
-
-        for(int i = 1; i < coords.size(); i++){
-                x = Character.getNumericValue(coords.get(i).charAt(0));
-                y = Character.getNumericValue(coords.get(i).charAt(2));
-                x1 = Character.getNumericValue(coords.get(i).charAt(4));
-                y1 = Character.getNumericValue(coords.get(i).charAt(6));
-                x3 = Character.getNumericValue(coords.get(i).charAt(12));
-                y3 = Character.getNumericValue(coords.get(i).charAt(14));
-            System.out.println(isTarget(x, y, x1, y1, 1, 1, x3, y3, 1, 1));
-            System.out.println();
-
+        int count = 0;
+        ArrayList<String> coords = new ArrayList<>();
+        ArrayList<Integer> curCoords = new ArrayList<>();
+        for(int i = 0; i <= n; i++){
+            coords.add(in.nextLine());
         }
+        coords.remove(coords.get(0));
 
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < coords.get(i).length()/2-1; j++) {
+                curCoords.add(Integer.valueOf(coords.get(i).split(" ")[j]));
+
+            }
+
+            if(isTarget(curCoords.get(0), curCoords.get(1), curCoords.get(2), curCoords.get(3), 1, 1, curCoords.get(6), curCoords.get(7), 1, 1)){
+                count = count + 1;
+            }
+            curCoords = new ArrayList<>();
+        }
+        System.out.println(count);
 
     }
 
 
-    public static boolean isTarget(long x, long y, long x1, long y1, long x2, long y2, long x3, long y3, long x4, long y4){
-            if(x1 < x && x < x3 && y1 < y && y < y3 ) return true;
-        else return false;
+
+
+    public static boolean isTarget(int x, int y, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4){
+            if(x1 < x && x < x3 && y1 < y && y < y3 ) return false;
+        else return true;
     }
     public static void run(){
 
---
+++ src/ACMP/Task13.java
@@ -0,0 +1,38 @@
+package ACMP;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Task13 {
+    public static void solve(Scanner in, PrintWriter out) {
+
+
+
+    }
+
+
+    public static void run(){
+
+        try (
+                Scanner in  = new Scanner(System.in);
+                PrintWriter out = new PrintWriter(System.out);
+        )
+        {
+            solve(in,out);
+        }
+
+
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        run();
+
+
+    }
+}
+
